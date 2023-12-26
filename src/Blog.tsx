42b12ca57f094ccf1fb1c319292f2121b748e11d
Ren Aoki <49857703+rensanrenren@users.noreply.github.com>
2023-12-26 21:18:15 +0900

    react test dev

---
+++ src/Blog.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ArticleProps from './Article';
+
+const Blog = () => {
+  return (
+    <ArticleProps
+      content={'こちらに記事の本文が入ります。'}
+      title={'タイトルです！'}
+    />
+  );
+};
